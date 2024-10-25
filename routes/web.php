4a14c2471192e17935c2638823ee228ea030ad2c
Narek Sahakyan <narsahakyan.work@gmail.com>
2024-10-25 18:06:04 +0400

    add cache

--- routes/web.php
+++ routes/web.php
@@ -7,13 +7,6 @@ Route::get('/', function () {
     return view('welcome');
 });
 
-//$text = (string)view('telegram.error', ['e' => $e]);
-$token = env('TELEGRAM_BOT_API_TOKEN');
-$id = (int)env('TELEGRAM_BOT_ID');
-
-$http = Http::post("https://api.telegram.org/bot$token/setWebhook",
-    [
-        'url' => 'https://t-bot.com/api/webhook',
-    ])->json();
-
-dd($http);
+Route::get('webhook-data', function() {
+    dd(\Illuminate\Support\Facades\Cache::get('webhook-data'));
+});
