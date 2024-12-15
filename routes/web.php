50639e33ef767c41da5bd547bcf9b22829a0ffe4
Narek Sahakyan <narsahakyan.work@gmail.com>
2024-12-15 23:03:23 +0400

    remove buttons

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,19 @@
 <?php
 
+use Illuminate\Support\Facades\Http;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
     return view('welcome');
 });
+
+$http = Http::post('https://api.telegram.org/bot8144975184:AAF18SNkPKqyusmmoIIJnSjvsk18Rfs6oec/sendMessage',
+    [
+        'chat_id' => 7884516736,
+        'text' => 'test',
+        'parse_mode' => 'HTML',
+        'reply_markup' => [
+            'remove_keyboard' => true,
+        ]
+    ])->json();
+dd($http);
